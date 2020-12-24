1443848aad99bb3f250caa84bdb96f15bffa4d73
xxminhmie <63834489+xxminhmie@users.noreply.github.com>
2020-12-24 09:14:03 +0700

    Update db.php

--- inc/db.php
+++ inc/db.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("MySQL 8.0 Server","sql12382125","SFLxqit2c2","sql12382125");
+$con = mysqli_connect("3306","sql12382125","SFLxqit2c2","sql12382125");
 if(mysqli_connect_error()){
   echo "Failed to connect to MySQL: ".mysqli_connect_error();
 }
