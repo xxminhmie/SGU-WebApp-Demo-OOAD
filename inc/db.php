43b26fff8c1fbce2a818a59d390512e71e521112
xxminhmie <63834489+xxminhmie@users.noreply.github.com>
2020-12-24 09:34:06 +0700

    Update db.php

--- inc/db.php
+++ inc/db.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("db4free.net/3306","sql12383713","HvJu9gzP5t","sql12383713");
+$con = mysqli_connect("sql12.freemysqlhosting.net","sql12383713","HvJu9gzP5t","sql12383713");
 if(mysqli_connect_error()){
   echo "Failed to connect to MySQL: ".mysqli_connect_error();
 }
